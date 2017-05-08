3e7890491c53700f7f38623da87360a09224b454
lmj <Chris_lmj@163.com>
2017-05-08 22:55:49 +0800

    add change01 angain

--- app/src/main/java/hexin/firstpushtogittest/MainActivity.java
+++ app/src/main/java/hexin/firstpushtogittest/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView contetn = (TextView) findViewById(R.id.content);
-//        contetn.setText("change01");
+        contetn.setText("change01");
     }
 }
