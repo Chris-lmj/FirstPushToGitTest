b3942647c913c287e64ee0a51047d2476db19343
lmj <Chris_lmj@163.com>
2017-05-08 23:08:32 +0800

    改成change03

--- app/src/main/java/hexin/firstpushtogittest/MainActivity.java
+++ app/src/main/java/hexin/firstpushtogittest/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //找到内容
         TextView contetn = (TextView) findViewById(R.id.content);
         contetn.setText("change01");
+        contetn.setText("change03");
     }
 }
